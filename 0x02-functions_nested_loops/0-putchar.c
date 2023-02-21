6b6fa5b120ad352ada019a78af5b6c88d8d9fa7c
Mahmoud Sheta <shetamahmoud463@gmail.com>
2023-02-21 06:09:31 -0800

    tow

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 	int main(void)
 	{
 	_putchar('_');
