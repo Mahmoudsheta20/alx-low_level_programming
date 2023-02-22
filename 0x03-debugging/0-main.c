687fdf4a9780cfb143e3fbcc775de3c82eae3397
Mahmoud Sheta <shetamahmoud463@gmail.com>
2023-02-22 08:41:23 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,16 +6,12 @@
  *
  * Return: Always 0 (Success)
  */
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
-	}
+}
