bd63408ac39b5abc527b3dd8dcf84480e974b8ce
Mahmoud Sheta <shetamahmoud463@gmail.com>
2023-02-27 01:56:05 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int sum;
+sum = strlen(s);
+	return (sum);
+}
